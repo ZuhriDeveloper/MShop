318a3da6f23bc2a11abb8fb29c4461225d2e8a82
Achmad Zuhri <achmad.zuhri@qualysoft-coe.com>
2022-04-20 01:58:06 +0700

    Add Cors Config

---
+++ Shared/MShop.Cors/IServiceCollectionExtentions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace MShop.Cors
+{
+    public static class IServiceCollectionExtentions
+    {
+        public static IServiceCollection AddCatsCors(this IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var corsString = configuration.GetSection("CorsString").Value;
+            var origins = corsString.Split(new[] { ',', ';' });
+
+#if DEBUG
+            Console.WriteLine("Origins:");
+            foreach (var o in origins)
+            {
+                Console.WriteLine("   " + o);
+            }
+#endif
+
+            return services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .WithOrigins(origins)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+        }
+    }
+}
